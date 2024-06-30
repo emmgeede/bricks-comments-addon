6379105dcaec0c4dfde7ee85443a78781ebd5906
emmgee <154229282+emmgeede@users.noreply.github.com>
2024-06-30 10:19:43 +0200

    feat: adidng skiplink to the top of the comments

--- bricks-comments-addon.php
+++ bricks-comments-addon.php
@@ -43,6 +43,7 @@ class Bricks_Comments_Addon {
 	
 	/**
 	 * Wrapping the comment elements with a wrapper
+	 * and add a skiplink to the topo f the comments
 	 *
 	 * @param $content
 	 * @param $post
@@ -59,6 +60,7 @@ class Bricks_Comments_Addon {
 		
 		$comments_title = $xpath->query( "//*[contains(concat(' ', normalize-space(@class), ' '), ' comments-title ')]" );
 		$comment_list   = $xpath->query( "//*[contains(concat(' ', normalize-space(@class), ' '), ' comment-list ')]" );
+		$comments       = $xpath->query( "//*[contains(concat(' ', normalize-space(@class), ' '), ' bricks-comments-inner ')]" );
 		
 		if ( $comments_title->length > 0 && $comment_list->length > 0 ) {
 			$wrapper = $doc->createElement( 'div' );
@@ -70,25 +72,27 @@ class Bricks_Comments_Addon {
 			$wrapper->appendChild( $comment_list->item( 0 ) );
 		}
 		
+		if ( $comments->length > 0 ) {
+			$link = $doc->createElement( 'a' );
+			$link->setAttribute( 'href', '#respond' );
+			$link->setAttribute( 'class', 'skip-link' );
+			$link->setAttribute( 'title', esc_html__( 'Skip to comment form', 'bricks' ) );
+			$link->nodeValue = esc_html__( 'Skip to comment form', 'bricks' );
+			
+			$comments->item( 0 )->parentNode->insertBefore( $link, $comments->item( 0 ) );
+		}
+		
 		$newHtml = $doc->saveHTML();
 		
 		return $newHtml;
 	}
 	
 	/**
-	 * Register the javascript and css files for the frontend
+	 * Register the css file for the frontend
 	 *
 	 * @return void
 	 */
 	public function bca_register_scripts() {
-		wp_register_script(
-			'bricks-comments-addon',
-			plugins_url( 'js/bricks-comments-addon.js', __FILE__ ),
-			[],
-			fileatime( __DIR__ . "/js/bricks-comments-addon.js" ),
-			true
-		);
-		
 		wp_register_style(
 			'bricks-comments-addon',
 			plugins_url( 'css/bricks-comments-addon.css', __FILE__ ),
@@ -158,25 +162,6 @@ class Bricks_Comments_Addon {
 		
 		return $controls;
 	}
-	
-	/**
-	 * Insert a value or key/value pair after a specific key in an array. If key doesn't exist, value is appended
-	 * to the end of the array.
-	 *
-	 * @param  array   $array
-	 * @param  string  $key
-	 * @param  array   $new
-	 *
-	 * @return array
-	 */
-	private function array_insert_after( array $array, $key, array $new ) {
-		$keys  = array_keys( $array );
-		$index = array_search( $key, $keys );
-		$pos   = false === $index ? count( $array ) : $index + 1;
-		
-		return array_merge( array_slice( $array, 0, $pos ), $new, array_slice( $array, $pos ) );
-	}
-	
 }
 
 $mg_addon = new Bricks_Comments_Addon();
